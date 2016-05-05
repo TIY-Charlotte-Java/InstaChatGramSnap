e41d9b37c7571d82f6b3c884bf2f554e5ec408c4
Michael Grill <Ultramar@michaels-mbp.apollosrc.root>
2016-05-05 13:45:56 -0400

    BasePoint1

---
+++ InstaChatGramSnap/src/main/java/com/theironyard/InstaChatGramSnap/InstaChatGramSnapApplication.java
@@ -0,0 +1,12 @@
+package com.theironyard.InstaChatGramSnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaChatGramSnapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaChatGramSnapApplication.class, args);
+	}
+}
---
+++ InstaChatGramSnap/src/test/java/com/theironyard/InstaChatGramSnap/InstaChatGramSnapApplicationTests.java
@@ -0,0 +1,18 @@
+package com.theironyard.InstaChatGramSnap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = InstaChatGramSnapApplication.class)
+@WebAppConfiguration
+public class InstaChatGramSnapApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
