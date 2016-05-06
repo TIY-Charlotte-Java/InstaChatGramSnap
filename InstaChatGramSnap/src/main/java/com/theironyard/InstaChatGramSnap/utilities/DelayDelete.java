5a08b4ea23797ec0c9be6f105c7890a6c870dfb5
Michael Grill <Ultramar@michaels-mbp.apollosrc.root>
2016-05-06 10:25:31 -0400

    ClassStuff

---
+++ InstaChatGramSnap/src/main/java/com/theironyard/InstaChatGramSnap/utilities/DelayDelete.java
@@ -0,0 +1,11 @@
+package com.theironyard.InstaChatGramSnap.utilities;
+
+/**
+ * Created by Ultramar on 5/6/16.
+ */
+public class DelayDelete implements Runnable {
+
+    public void run() {
+        
+    }
+}
